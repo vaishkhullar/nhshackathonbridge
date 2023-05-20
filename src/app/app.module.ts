96d7462df559ddacc6d98dda1348d203e8d07a9e
nick.rees91 <nick.rees91@gmail.com>
2023-05-20 15:31:31 +0100

    Added Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { IonicModule } from "@ionic/angular";
+import { AppComponent } from "./app.component";
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, IonicModule.forRoot(), HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    IonicModule.forRoot(),
+    HttpClientModule,
+    AppRoutingModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
