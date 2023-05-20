059e77243ae7595927e7d8fe4593ee60af30c5ac
nick.rees91 <nick.rees91@gmail.com>
2023-05-20 16:49:12 +0100

    Added API integration

--- src/app/pages/patient-updates/patient-updates.page.ts
+++ src/app/pages/patient-updates/patient-updates.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-import { NavController } from "@ionic/angular";
+import { Subscription } from "rxjs";
+
+//Models
+import { PatientStatus } from "src/app/models/patient-status.model";
+
+//Services
+import { PatientStatusAPIService } from "src/app/services/api/patient-status.service";
 
 @Component({
   selector: "app-patient-updates",
@@ -7,7 +13,30 @@ import { NavController } from "@ionic/angular";
   styleUrls: ["./patient-updates.page.scss"],
 })
 export class PatientUpdatesPage implements OnInit {
-  constructor(private navCtrl: NavController) {}
+  private subscriptions: Subscription[] = [];
+  public patientUpdates: PatientStatus[] = [];
 
-  ngOnInit() {}
+  constructor(private patientStatusAPI: PatientStatusAPIService) {}
+
+  ngOnInit() {
+    console.log("Test");
+    this.fetchPatientStatusInformation(35);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
+
+  public async fetchPatientStatusInformation(patientId: number) {
+    const patientStatusSub = this.patientStatusAPI
+      .getPatientStatus(patientId)
+      .subscribe((patientStatus: PatientStatus[]) => {
+        if (patientStatus && patientStatus.length > 0) {
+          console.log(patientStatus);
+          this.patientUpdates = patientStatus;
+        }
+      });
+    if (!this.subscriptions.includes(patientStatusSub))
+      this.subscriptions.push(patientStatusSub);
+  }
 }
