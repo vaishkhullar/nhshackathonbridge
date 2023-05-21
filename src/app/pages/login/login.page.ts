e9157de17174f8cfbabf48f6c54a772ec2cf3ab5
NickRees91 <60291957+NickRees91@users.noreply.github.com>
2023-05-21 10:35:55 +0100

    2 files modified

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { NavController } from "@ionic/angular";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.page.html",
-  styleUrls: ["./login.page.scss"],
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-   //Form Related
-   public loginForm: FormGroup;
+  //Form Related
+  public loginForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
     this.loginForm = this.formBuilder.group({
-      firstName: ['', Validators.compose([Validators.required])]
+      firstName: ['', Validators.compose([Validators.required])],
     });
-
+    console.log(this.loginForm.value);
   }
 
+  public insertUpdate(){
+    api.insert({
+      happy: this.loginForm.value["happy"]
+    })
+  }
   ngOnInit() {}
 }
